acf05eaaa091893c8bf50a8eb8dcfed749ecee21
Craig Knoblauch <craigknoblauch@gmail.com>
2019-01-16 12:57:36 -0500

    stuff for py unit testing

---
+++ functions/main.py
@@ -0,0 +1,11 @@
+# Simple functions so I can work with pytest
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def isPositive(a):
+    return a > 0
+
---
+++ test/maintest.py
@@ -0,0 +1,9 @@
+from functions.main import *
+
+assert add(1, 2) == 3
+
+assert sub(1, 2) == -1
+
+assert isPositive(3)
+
+assert not isPositive(-3)
