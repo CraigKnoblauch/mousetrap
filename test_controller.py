f3236efe83428159f58bd41bab8a2cd6fac33e73
Craig Knoblauch <craigknoblauch@gmail.com>
2019-01-31 12:30:40 -0700

    dummy tests

--- test_controller.py
+++
@@ -1,8 +0,0 @@
-from controller import Controller
-
-ctl = Controller()
-
-def test_selectRoster():
-    assert ctl.selectRoster(1)
-
-# TODO, uhhhh, I don't know how to test this thing without maybe mock objects?
